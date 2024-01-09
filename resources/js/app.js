4aa379689736e85815fda72ed330fe0b2d102360
selch9876 <32863267+selch9876@users.noreply.github.com>
2024-01-09 15:24:23 +0300

    Breeze Installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,23 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
-import MainLayout from './Layouts/MainLayout.vue'
-//import {ZiggyVue} from 'ziggy'
-import '../css/app.css'
 
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-  progress: {
-    delay: 0,
-    color: '#29',
-    includeCSS: true,
-    showSpinner: true,
-  },
-  
-  resolve: name => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-    let page = pages[`./Pages/${name}.vue`]
-    page.default.layout = page.default.layout || MainLayout
-    return page
-  },
-  setup({ el, App, props, plugin }) {
-    createApp({ render: () => h(App, props) })
-      .use(plugin)
-      .use(ZiggyVue)
-      .mount(el)
-  },
-})
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
